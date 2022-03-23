33bd42766ee490407dcf293f3c833bce353bb660
SuzukiDaishi <zukky.rikugame@gmail.com>
2022-03-24 01:29:19 +0900

    fix: hparams.py

--- hparams.py
+++ hparams.py
@@ -6,6 +6,10 @@ class hparams:
     dim_pre = 512
     freq = 32
 
+    hop_length = 256
+    seq_len_factor = 64
+    seq_len = seq_len_factor * hop_length
+
     ## world vocoder
     sp_max = 4.3340
     sp_min = -38.6925
